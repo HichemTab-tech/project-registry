d0a5e94d12c9050a34bbb6efa93773e5675204be
HichemTab-tech <konanhichemsinshi@gmail.com>
2026-01-10 00:37:06 +0100

    enhance list command to include template content and improved output formatting

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,11 +1,18 @@
-import {Command} from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
 
 import {getAllTemplates} from '../utils/config.js'
 
 class List extends Command {
     static description = 'List all registered templates'
 
-    static examples = ['<%= config.bin %> <%= command.id %>']
+    static examples = [
+        '<%= config.bin %> <%= command.id %>',
+        '<%= config.bin %> <%= command.id %> -c'
+    ]
+
+    static flags = {
+        "with-content": Flags.string({char: 'c', description: 'With template content'}),
+    }
 
     async run(): Promise<void> {
         const templates = getAllTemplates()
@@ -18,11 +25,22 @@ class List extends Command {
 
         for (const name of names) {
             const template = templates[name]
+            let result = name;
             if (template.description) {
-                this.log(`${name} - ${template.description}`)
-            } else {
-                this.log(name)
+                if (template.description.length > 100) template.description = template.description.slice(0, 100) + '...'
+                result += ` - ${template.description}`
             }
+            else{
+                result += ` - No description`
+            }
+
+            if (template.commands) {
+                let commands = template.commands.join(' && ')
+                if (commands.length > 100) commands = commands.slice(0, 100) + '...'
+                result += ` - ${commands}`
+            }
+
+            this.log(result)
         }
     }
 }
